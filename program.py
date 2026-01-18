1d70c1d02bdce928bde60ff5c55361ccaa3a9f8c
maheer <abnaymaher@gmail.com>
2026-01-18 03:39:48 +0100

    Add files via upload

---
+++ program.py
@@ -0,0 +1,38 @@
+import requests
+import json
+import asyncio
+import websockets
+
+
+SERVER_WS = "ws://127.0.0.1:8765"
+
+def get_ip_info():
+   r = requests.get("https://ipinfo.io/json",timeout=5)
+   return r.json()
+
+def is_morocco(info):
+    return info.get("country") == "MA"
+
+async def send(datainfo):
+    async with websockets.connect(SERVER_WS) as ws:
+        await ws.send(json.dumps(datainfo))
+
+
+def main():
+    info = get_ip_info()
+    if not is_morocco(info):
+        print('[-] user not in morocco')
+        return #stop
+    lat, lon = map(float,info["loc"].split(","))
+    datainfo = {
+        "country":"MA",
+        "City":info.get("city"),
+        "org":info.get("org"),
+        "lat":lat,
+        "lon":lon
+    }
+    asyncio.run(send(datainfo))
+    print("[+] sended")
+
+if __name__ == "__main__":
+    main()
